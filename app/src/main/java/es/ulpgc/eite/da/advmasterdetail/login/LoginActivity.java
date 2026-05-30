d55f99a188e5e9cf3d258103b0ffc4988f448fe4
danielaHerrera <danalaycloe@gmail.com>
2026-05-30 13:19:28 +0100

    Implementado el registro e inicio de sesión real utilizando Room y la tabla de usuarios.

--- app/src/main/java/es/ulpgc/eite/da/advmasterdetail/login/LoginActivity.java
+++ app/src/main/java/es/ulpgc/eite/da/advmasterdetail/login/LoginActivity.java
@@ -3,15 +3,22 @@ package es.ulpgc.eite.da.advmasterdetail.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import es.ulpgc.eite.da.advmasterdetail.R;
+import es.ulpgc.eite.da.advmasterdetail.database.CatalogDatabase;
+import es.ulpgc.eite.da.advmasterdetail.database.UserEntity;
 import es.ulpgc.eite.da.advmasterdetail.home.HomeActivity;
 import es.ulpgc.eite.da.advmasterdetail.register.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private CatalogDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +26,52 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.login);
 
+        database = Room.databaseBuilder(getApplicationContext(), CatalogDatabase.class, "catalog.db").allowMainThreadQueries().build(); //Abre el Room
+
+        //ahora lee lo que se ha escrito en el campo de texto
+        EditText usernameInput = findViewById(R.id.usernameInput);
+
+        EditText passwordInput = findViewById(R.id.passwordInput);
+
         Button loginButton = findViewById(R.id.loginButton);
+
         TextView registerText = findViewById(R.id.registerText);
+
         TextView invitadoText = findViewById(R.id.invitadoText);
 
         loginButton.setOnClickListener(view -> {
-            Intent intent = new Intent(this, HomeActivity.class);
-            startActivity(intent);
+
+            String username = usernameInput.getText().toString();
+
+            String password =passwordInput.getText().toString();
+
+            UserEntity user = database.userDao().login(username, password);
+
+            if(user != null){
+
+                Toast.makeText(this, "Inicio de sesión correcto", Toast.LENGTH_SHORT).show();//Muestra un mensaje de inicio de sesión correcto
+
+                Intent intent = new Intent(this, HomeActivity.class);
+
+                startActivity(intent);
+
+            } else {
+
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();//Muestra un mensaje de eeror de inicio de sesión
+            }
         });
 
         registerText.setOnClickListener(view -> {
+
             Intent intent = new Intent(this, RegisterActivity.class);
+
             startActivity(intent);
         });
 
         invitadoText.setOnClickListener(view -> {
+
             Intent intent = new Intent(this, HomeActivity.class);
+
             startActivity(intent);
         });
     }
--- app/src/main/java/es/ulpgc/eite/da/advmasterdetail/register/RegisterActivity.java
+++ app/src/main/java/es/ulpgc/eite/da/advmasterdetail/register/RegisterActivity.java
@@ -3,13 +3,20 @@ package es.ulpgc.eite.da.advmasterdetail.register;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import es.ulpgc.eite.da.advmasterdetail.R;
+import es.ulpgc.eite.da.advmasterdetail.database.CatalogDatabase;
+import es.ulpgc.eite.da.advmasterdetail.database.UserEntity;
 import es.ulpgc.eite.da.advmasterdetail.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private CatalogDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +24,39 @@ public class RegisterActivity extends AppCompatActivity {
 
         setContentView(R.layout.register);
 
+        database = Room.databaseBuilder(getApplicationContext(), CatalogDatabase.class, "catalog.db").allowMainThreadQueries().build();//Abre el Room
+
+        EditText usernameInput = findViewById(R.id.registerUsernameInput);//lee lo que se ha escrito en el campo de texto
+
+        EditText passwordInput = findViewById(R.id.registerPasswordInput);
+
         Button registerButton = findViewById(R.id.registerButton);
 
         registerButton.setOnClickListener(view -> {
+
+            String username = usernameInput.getText().toString();
+
+            String password = passwordInput.getText().toString();
+
+            if(username.isEmpty() || password.isEmpty()){
+
+                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show();
+
+                return;
+            }
+            UserEntity user = new UserEntity(); //Crea un nuevo usuario
+
+            user.username = username;
+            user.password = password;
+
+            database.userDao().insertUser(user);//Guarda el usuario en la tabla de datos de users
+
+            Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show();
+
             Intent intent = new Intent(this, LoginActivity.class);
+
             startActivity(intent);
+
             finish();
         });
     }
