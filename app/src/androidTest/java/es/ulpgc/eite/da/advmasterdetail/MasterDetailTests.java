2e4ec53092de015b5a06ac7b811789de1c62c5be
danielaHerrera <danalaycloe@gmail.com>
2026-06-03 13:15:50 +0100

    test de rotación añadidos

--- app/src/androidTest/java/es/ulpgc/eite/da/advmasterdetail/MasterDetailTests.java
+++ app/src/androidTest/java/es/ulpgc/eite/da/advmasterdetail/MasterDetailTests.java
@@ -14,7 +14,7 @@ import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
-
+import android.content.pm.ActivityInfo;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -197,13 +197,85 @@ public class MasterDetailTests {
     @Test
     public void test08LoggedUserCanAddFavorite() {
 
-        onView(withId(R.id.usernameInput))
-                .perform(replaceText("david123"), closeSoftKeyboard());
+        onView(withId(R.id.usernameInput)).perform(replaceText("david123"), closeSoftKeyboard());
 
-        onView(withId(R.id.passwordInput))
-                .perform(replaceText("1234"), closeSoftKeyboard());
+        onView(withId(R.id.passwordInput)).perform(replaceText("1234"), closeSoftKeyboard());
 
         onView(withId(R.id.loginButton)).perform(click());
+
+        waitForData();
+
+        onView(withId(R.id.communicationsButton)).perform(click());
+
+        waitForData();
+
+        onView(new RecyclerViewMatcher(R.id.product_recycler).atPositionOnView(0, R.id.product_name)).perform(click());
+
+        waitForData();
+
+        onView(withId(R.id.favoriteButton)).perform(click());
+
+        waitForData();
+
+        onView(withId(R.id.favoriteButton)).check(matches(withText("Quitar de favoritos")));
+    }
+    private void rotateScreen() {
+
+        if(testRule.getActivity().getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+
+            testRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        } else {
+
+            testRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+
+        waitForData();
+    }
+    @Test
+    public void test09RotationKeepsLoginData() {
+
+        onView(withId(R.id.usernameInput)).perform(replaceText("Daniela"), closeSoftKeyboard());
+
+        onView(withId(R.id.passwordInput)).perform(replaceText("1234"), closeSoftKeyboard());
+
+        rotateScreen();
+
+        onView(withId(R.id.usernameInput)).check(matches(withText("Daniela")));
+
+        onView(withId(R.id.passwordInput)).check(matches(withText("1234")));
+    }
+    @Test
+    public void test10RotationKeepsRegisterData() {
+
+        onView(withId(R.id.registerText)).perform(click());
+
+        waitForData();
+
+        onView(withId(R.id.registerUsernameInput))
+                .perform(replaceText("usuario"), closeSoftKeyboard());
+
+        onView(withId(R.id.registerPasswordInput))
+                .perform(replaceText("1234"), closeSoftKeyboard());
+
+        onView(withId(R.id.registerConfirmPasswordInput))
+                .perform(replaceText("1234"), closeSoftKeyboard());
+
+        rotateScreen();
+
+        onView(withId(R.id.registerUsernameInput))
+                .check(matches(withText("usuario")));
+
+        onView(withId(R.id.registerPasswordInput))
+                .check(matches(withText("1234")));
+
+        onView(withId(R.id.registerConfirmPasswordInput))
+                .check(matches(withText("1234")));
+    }
+    @Test
+    public void test11RotationKeepsDetailData() {
+
+        onView(withId(R.id.invitadoText)).perform(click());
 
         waitForData();
 
@@ -217,11 +289,14 @@ public class MasterDetailTests {
 
         waitForData();
 
-        onView(withId(R.id.favoriteButton)).perform(click());
+        rotateScreen();
 
-        waitForData();
+        onView(withId(R.id.detailName))
+                .check(matches(withText("Starlink")));
 
-        onView(withId(R.id.favoriteButton))
-                .check(matches(withText("Quitar de favoritos")));
+        onView(withId(R.id.detailAgency))
+                .check(matches(withText("🏢 Agencia: SpaceX")));
     }
+
+
 }
